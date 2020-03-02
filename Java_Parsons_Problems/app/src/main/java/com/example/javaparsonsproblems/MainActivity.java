ec7b1f5a2609b5bf2776b79cff48f97b023b2636
dym4xion <dym4xion@users.noreply.github.com>
2020-03-02 22:50:04 +0000

    Few more problems

--- Java_Parsons_Problems/app/src/main/java/com/example/javaparsonsproblems/MainActivity.java
+++ Java_Parsons_Problems/app/src/main/java/com/example/javaparsonsproblems/MainActivity.java
@@ -127,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
     public int[][] getVariantsMatrix(){
         int[][] lvlMatrix = {
    //skill level:1,2,3,4,5,6,7,8,9,10
-                {3,2,1,2,1,1,1,1,1,1}, //IO
+                {3,3,3,3,1,1,1,1,1,1}, //IO
                 {1,0,0,0,0,0,0,0,0,0}, //VAR
                 {1,0,0,0,0,0,0,0,0,0}, //CON
                 {1,0,0,0,0,0,0,0,0,0}, //DS
